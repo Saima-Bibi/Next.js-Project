f61a5926b237866e03a6f7db7e50502a285b7520
Saima Bibi <bibi33243@gmail.com>
2025-07-07 10:17:15 +0500

    implemented SubmitPost API and integrated it to the frontend

--- src/app/api/feedback/route.js
+++ src/app/api/feedback/route.js
@@ -1,5 +1,6 @@
 import dbConnection from "@/lib/db_connection";
 import appModel from "@/model/apps";
+import feedbackModel from "@/model/feedback";
 import { NextResponse } from "next/server";
 
 
@@ -7,10 +8,27 @@ export async function GET(req){
     try {
         await dbConnection()
     const appData = await appModel.find();
-    return NextResponse.json({message:'demo api is working', appData})
+    return NextResponse.json({message:'Fetching App data', appData})
 
     } catch (error) {
         console.log(error.message)
+         return NextResponse.json({message:'Internal Server Error'}, {status: 500})
+    }
+}
+
+export async function POST(req){
+    try {
+        const {appId,feedback,userId} =  await req.json()
+
+        
+        const newFeedback = new feedbackModel({appId,feedback,userId})
+        await newFeedback.save()
+         console.log('log here',appId,feedback,userId)
+         
+      return NextResponse.json({message:'Feedback received',newFeedback})
+        
+    } catch (error) {
+         console.log(error.message)
          return NextResponse.json({message:'Internal Server Error'}, {status: 500})
     }
 }
--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -6,12 +6,45 @@ import axios from 'axios'
 export default function Feedback() {
  const[name,setName]= useState('Salma')
  const[apps,setApp]= useState([])
+ const[data,setData]= useState({
+  appId:'',
+  feedback:'',
+  userId:'686a82245315f301a5d276c6'
+ })
  const router = useRouter()
 
  const getData = async() =>{
  axios.get('/api/feedback')
-      .then((res) => setApp(res.data.data))
+      .then((res) => setApp(res.data.appData))
       .catch((err) => console.error('Failed to fetch apps:', err));
+ }
+
+ const handleChange= (e)=>{
+   setData({
+    ...data,
+   [e.target.name]: e.target.value}
+   )
+ }
+
+ const handleSubmit=async(e)=>{
+  e.preventDefault()
+  try {
+     if (!data.appId || !data.feedback) {
+    alert("Please select an app and write feedback.");
+    return;
+  }
+  const res = await axios.post('/api/feedback', data);
+    alert(res?.data?.message);
+   setData({
+      appId: '',
+      feedback: '',
+      userId: '686a82245315f301a5d276c6'
+    });
+    
+  } catch (error) {
+    console.error('Failed to submit feedback',error)
+  }
+
  }
 
  useEffect(()=>{
@@ -28,7 +61,7 @@ export default function Feedback() {
      <h1 className="text-5xl font-bold underline">
       Hello world!
     </h1> */}
-    <button className="btn" onClick={()=>document.getElementById('my_modal_1').showModal()}>Feedback</button>
+    <button className="btn" onClick={()=>document.getElementById('my_modal_1').showModal()}>Give Feedback</button>
 <dialog id="my_modal_1" className="modal">
   <div className="modal-box">
     <h3 className="font-bold text-lg text-center">Hello!</h3>
@@ -37,23 +70,25 @@ export default function Feedback() {
     <fieldset className="mb-2">
  
   
-    <select defaultValue="Pick an app" className="select ml-16">
-    <option disabled={true}>Pick an app</option>
+    <select  className="select ml-16" name='appId' value={data.appId} onChange={handleChange}>
+     <option value="" disabled>
+    Pick an app
+  </option>
     {apps.map((app)=>(
-      <option key={app._id} value={app.name}>{app.name}</option>
+      <option key={app._id} value={app._id}>{app.name}</option>
     ))}
     
   </select>
 
 </fieldset>
 
-    <input type="text" placeholder="Type here" className="input ml-16" />
+    <input type="text" placeholder="Type here" className="input ml-16" name='feedback' value={data.feedback} onChange={handleChange}/>
     <div className="modal-action">
       <form method="dialog">
         {/* if there is a button in form, it will close the modal */}
         <button className="btn">Close</button>
       </form>
-      <button className="btn">Submit</button>
+      <button className="btn" onClick={handleSubmit}>Submit</button>
     </div>
   </div>
 </dialog>
