1565bebb0ec3dd827184eb06b334238ddd9dcf0d
Saima Bibi <bibi33243@gmail.com>
2025-07-08 12:32:41 +0500

    practiced image optimization using Image component of the Next.js

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image'
+import img from '/public/next.svg'
 
 function page() {
   return (
     <div>
       <h1>Login Page</h1>
+      <Image src={img} height={400} width={400} alt='image'/>
+      <Image src='https://images.pexels.com/photos/1563356/pexels-photo-1563356.jpeg?_gl=1*8a07on*_ga*MTUxMDQ3MTcxNS4xNzQ1Njc4NTg0*_ga_8JE65Q40S6*czE3NTE5NTg5NDQkbzIkZzEkdDE3NTE5NTk2MjkkajI5JGwwJGgw' height={400} width={400} alt='image'/>
     </div>
   )
 }
